6849807fba3a025e3dd7e6b861fae8761d6daf86
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-25 13:19:04 -0500

    fixed index

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ window.onscroll = function() {scroll()} //when scrolling call function scroll()
 let counter = 0 //kinda like a loop variable
 let quoteCounter = 0
 let parentQuoteCounter = 0
+let imgCounter = 0
 
 let elements = document.getElementsByClassName("text") //elements w/ class text
 objects = Array.prototype.slice.call(elements) //turn HTML collection into arrya
@@ -15,6 +16,10 @@ let quotePositions = quoteObjects.map(i => i.offsetTop)
 let parentElements = document.getElementsByClassName("quote")
 parentObjects = Array.prototype.slice.call(parentElements)
 let parentPositions = parentObjects.map(i => i.offsetTop)
+
+let imgElements = document.getElementsByClassName("big_img")
+imgObjects = Array.prototype.slice.call(imgElements)
+let imgPositions = imgObjects.map(i => i.offsetTop)
 
 let navbar = document.getElementById("navbar") //get position of the navbar
 
@@ -36,12 +41,19 @@ function scroll() {
     if (positions[counter] <= window.pageYOffset+window.screen.height*0.8) scroll() //if scrolled through more than one element recurse to load multiple elements at once
   }
   if (window.pageYOffset+window.screen.height*0.6 >= quotePositions[quoteCounter] + parentPositions[parentQuoteCounter]) { //if bottom of screen*0.8 is below elements[counter]
-    console.log("OFFSET", window.pageYOffset)
-    console.log(quotePositions[quoteCounter])
     quoteElements[quoteCounter].classList.add("textappear") //make text appear
     quoteElements[quoteCounter].classList.remove("texttrans") //remove transparent class
     quoteCounter ++ //increment counter to next element
     parentQuoteCounter ++
     if (quotePositions[quoteCounter] <= window.pageYOffset+window.screen.height*0.5) scroll() //if scrolled through more than one element recurse to load multiple elements at once
+  }
+  if (window.pageYOffset+window.screen.height*0.6 >= imgPositions[imgCounter] + parentPositions[parentQuoteCounter]) {
+    console.log("OFFSET", window.pageYOffset)
+    console.log(quotePositions[quoteCounter])
+
+    imgElements[imgCounter].classList.add("blur")
+    imgElements[imgCounter].classList.remove("noblur")
+    imgCounter++
+    if (window.pageYOffset+window.screen.height*0.6 >= imgPositions[imgCounter] + parentPositions[parentQuoteCounter]) scroll()
   }
 }
