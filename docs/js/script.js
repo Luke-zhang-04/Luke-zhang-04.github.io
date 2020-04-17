1a849c915bdba76a082f15e4438ab118087d7104
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-17 15:03:12 -0400

    added navbar

---
+++ docs/js/script.js
@@ -0,0 +1,42 @@
+/*
+	luke-zhang-04.github.io
+    Copyright (C) 2020  Luke Zhang
+    
+    https://github.com/Luke-zhang-04
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+const print = console.log
+
+let toggled = false
+
+window.onscroll = () => {scroll()};
+
+// Get the navbar
+const navbar = document.querySelector(".navbar")
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scroll() {
+    if (window.pageYOffset >= 130) {
+        navbar.classList.add("navbar-background")
+        navbar.classList.add("navbar-dark")
+        navbar.classList.remove("navbar-light")
+        navbar.classList.remove("navbar-transparent")
+        navbar.classList.remove("navbar-default")
+        toggled = true
+    } else if (toggled) {
+        navbar.classList.remove("navbar-background")
+        navbar.classList.add("navbar-transparent")
+        navbar.classList.add("navbar-light")
+        navbar.classList.remove("navbar-dark")
+    }
+}
