975456d2ae1d40415902bc0d7e5d3a41c4428104
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-24 10:43:35 -0400

    added filters

--- gh-pages/js/portfolio.js
+++ gh-pages/js/portfolio.js
@@ -16,6 +16,9 @@
 
 let data = {projects: {}, collections: {}} //data for projects
 
+let activeFilters = []
+let filters = []
+
 window.onresize = function() { //resive image seperation div
     document.getElementById("splash").style.height = (
         window.innerWidth*0.66666666666 - navbar.offsetHeight - 26
@@ -24,6 +27,151 @@ window.onresize = function() { //resive image seperation div
 document.getElementById("splash").style.height = (
     window.innerWidth*0.66666666666 - navbar.offsetHeight - 26
 ).toString() + "px"
+
+function clearProjects() {
+    const collectionContainer = document.getElementById("collections")
+    const projectContainer = document.getElementById("projects")
+    const collectionColumns = collectionContainer.querySelector(".row").children
+    const projectColumns = projectContainer.querySelector(".row").children
+
+    for (let i of collectionColumns) {
+        i.innerHTML = ""
+        print(i)
+    }
+    for (let i of projectColumns) {
+        i.innerHTML = ""
+        print(i)
+    }
+    displayExistingProjects()
+}
+
+function includesTags(tags, filters) {
+    for (i of tags) {
+        if (filters.includes(i)) {
+            return true
+        }
+    }
+    return false
+}
+
+function displayExistingProjects() {
+    let counter
+    counter = 0
+    for ([id, val] of Object.entries(data.projects)) {
+        if (!includesTags(val.oldTags, activeFilters)) {
+            let url = val.imgurl
+            let tags = ""
+            let description = ""
+
+            for (i = 0; i < val.tags.length; i++) { //format tags and make sure they aren't too long
+                if (i >= 5) {
+                    tags += " . . ."
+                    break
+                } else if (i == 4 || i == val.tags.length-1) {
+                    tags += `${val.tags[i]}`
+                } else {
+                    tags += `${val.tags[i]}, `
+                }
+            }
+
+            for (i = 0; i < val.description.split(" ").length; i++) { //format description
+                if (i >= 20) {
+                    description += " . . ."
+                    break
+                } else {
+                    description += val.description.split(" ")[i].replace(`\\n`, "<br/>").replace(`\\n`, "") + " "
+                }
+            }
+
+            let column = document.getElementById("projects").querySelector(".row").children[counter%3]
+
+            column.insertAdjacentHTML("beforeend", //insert tags and a card
+                `<div class="card port-card" style="display: inline-block; width: 100%" data-toggle="modal" data-target="#projectModal" data-name="projects.${id}">
+                    <img class="card-img-top" src="${url}" alt="Card image">
+                    <div class="card-img-overlay card-cover"></div>
+                    <div class="card-img-overlay">
+                        <h4 class="card-title">${(id).charAt(0).toUpperCase() + (id).slice(1)}</h4>
+                        <p>${description}</p>
+                        <p class="card-text">${tags}</p>
+                    </div>
+                </div>`
+            )
+            counter ++
+        }
+    }
+    counter = 0
+    for ([id, val] of Object.entries(data.collections)) {
+        if (!includesTags(val.oldTags, activeFilters)) {
+            if (!includesTags(val.oldTags, activeFilters)) {
+                let url = val.imgurl
+                let tags = ""
+                let description = ""
+    
+                for (i = 0; i < val.tags.length; i++) { //format tags and make sure they aren't too long
+                    if (i >= 5) {
+                        tags += " . . ."
+                        break
+                    } else if (i == 4 || i == val.tags.length-1) {
+                        tags += `${val.tags[i]}`
+                    } else {
+                        tags += `${val.tags[i]}, `
+                    }
+                }
+    
+                for (i = 0; i < val.description.split(" ").length; i++) { //format description
+                    if (i >= 20) {
+                        description += " . . ."
+                        break
+                    } else {
+                        description += val.description.split(" ")[i].replace(`\\n`, "<br/>").replace(`\\n`, "") + " "
+                    }
+                }
+    
+                let column = document.getElementById("collections").querySelector(".row").children[counter%3]
+    
+                column.insertAdjacentHTML("beforeend", //insert tags and a card
+                    `<div class="card port-card" style="display: inline-block; width: 100%" data-toggle="modal" data-target="#projectModal" data-name="projects.${id}">
+                        <img class="card-img-top" src="${url}" alt="Card image">
+                        <div class="card-img-overlay card-cover"></div>
+                        <div class="card-img-overlay">
+                            <h4 class="card-title">${(id).charAt(0).toUpperCase() + (id).slice(1)}</h4>
+                            <p>${description}</p>
+                            <p class="card-text">${tags}</p>
+                        </div>
+                    </div>`
+                )
+                counter ++
+            }
+        }
+    }
+}
+
+function addFilters() {
+    const filterContainer = document.getElementById("filters")
+    for (i of filters.sort(function (a, b) {
+        if (a.toLowerCase() > b.toLowerCase()) {
+            return 1;
+        }
+        if (b.toLowerCase() > a.toLowerCase()) {
+            return -1;
+        }
+        return 0;
+    })) {
+        filterContainer.insertAdjacentHTML("beforeend", `<div id="filter-${i}" class="card-body"><i class="fas fa-check"></i> ${i}</div>`)
+        let cur = i
+        document.getElementById("filter-" + i).addEventListener("click", e => {
+            if (!activeFilters.includes(cur)) {
+                e.target.innerHTML = `<i class="fas fa-times"></i> ${cur}`
+                activeFilters.push(cur)
+            } else {
+                e.target.innerHTML = `<i class="fas fa-check"></i> ${cur}`
+                activeFilters = activeFilters.filter(function(value) {
+                    return value !== cur
+                })
+            }
+        })
+    }
+}
 
 function displayProjects() { //displays project cards
     const projectContainer = document.getElementById("projects")
@@ -37,9 +185,16 @@ function displayProjects() { //displays project cards
         snapshot.docs.forEach(doc => {
             let imgurl = storageRef.child(doc.data().file) //get image URL
             let column = projectColumns.getElementsByClassName("proj-col")[counter%3] //column reference
+
+            for (i of doc.data().tags) {
+                if (!filters.includes(i)) {
+                    filters.push(i)
+                }
+            }
             
             imgurl.getDownloadURL().then(url => {
                 data.projects[doc.id] = doc.data() //insert doc data
+                data.projects[doc.id].oldTags = doc.data().tags
                 data.projects[doc.id]["imgurl"] = url //insert image url
 
                 for (i = 0; i < data.projects[doc.id].tags.length; i++) { //capitalize tags
@@ -95,8 +250,15 @@ function displayProjects() { //displays project cards
                 let imgurl = storageRef.child(doc.data().file) //get image URL
                 let column = collectionColumns.getElementsByClassName("proj-col")[counter%3] //column reference
 
+                for (i of doc.data().tags) {
+                    if (!filters.includes(i)) {
+                        filters.push(i)
+                    }
+                }
+
                 imgurl.getDownloadURL().then(url => {
                     data.collections[doc.id] = doc.data() //insert doc data
+                    data.collections[doc.id].oldTags = doc.data().tags
                     data.collections[doc.id]["imgurl"] = url //insert image URL
 
                     for (i = 0; i < data.collections[doc.id].tags.length; i++) { //capitalize tags
@@ -140,6 +302,8 @@ function displayProjects() { //displays project cards
                 })
                 counter ++
             })
+        }).then(() => {
+            addFilters()
         })
     })
 }
