8a43b9e86b00d37f6677fe177d10c85dd69b8d83
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-19 23:50:53 -0400

    add browserify to make code browser compatible
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ nodejs/home.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var print_1 = __importDefault(require("./print"));
+var testFunc = function (param) { return print_1["default"](param); };
+testFunc("ok boomer");
+//# sourceMappingURL=home.js.map
---
+++ nodejs/print.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var stdout = function (item) { return console.log(item); };
+exports["default"] = stdout;
+//# sourceMappingURL=print.js.map
