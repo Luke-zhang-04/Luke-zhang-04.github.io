684ac996e215db05a0a6e999e1482a8710a67c8d
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-02 20:16:40 -0400

    inline some styles

--- src/components/pages/home/contact.svelte
+++ src/components/pages/home/contact.svelte
@@ -4,7 +4,78 @@ Luke Zhang's developer portfolio | https://Luke-zhang-04.github.io
 License: BSD-3-Clause
 Copyright (C) 2020 - 2021 Luke Zhang
  -->
-<style lang="scss" src="./contact.scss"></style>
+<style lang="scss">
+    @use "src/styles/variables";
+
+    $transition-timing: map-get(variables.$timings, "ease-in-out");
+
+    .contact-title {
+        position: relative;
+        margin-top: 10vh;
+    }
+
+    .contact-container {
+        height: max-content;
+
+        ul {
+            position: relative;
+            display: flex;
+            justify-content: space-around;
+            width: 100%;
+            margin: 10vh 0;
+
+            li {
+                position: relative;
+                list-style: none;
+                cursor: pointer;
+                transform: translateX(-25%);
+
+                &::before {
+                    font-family: fontawesome, sans-serif;
+                    position: absolute;
+                    top: -3vw;
+                    left: 0;
+                    font-size: 12vw;
+                    height: 0;
+                    overflow: hidden;
+                    transition: height 0.5s $transition-timing;
+                }
+
+                &:hover::before {
+                    height: 150%;
+                }
+
+                &:nth-child(1)::before {
+                    content: "\f2b6";
+                    color: variables.$danger;
+                    border-bottom: 0.25rem solid;
+                }
+
+                &:nth-child(2)::before {
+                    content: "\f09b";
+                    color: black;
+                    border-bottom: 0.25rem solid;
+                }
+
+                &:nth-child(3)::before {
+                    content: "\f08c";
+                    color: #2867b2;
+                    border-bottom: 0.25rem solid;
+                }
+
+                a {
+                    text-decoration: none;
+
+                    .fab,
+                    .far {
+                        font-size: 12vw;
+                        color: mix(variables.$light, variables.$dark, 75%);
+                    }
+                }
+            }
+        }
+    }
+</style>
 
 <script lang="ts">
     const data: {href: string; iconClass: string}[] = [
