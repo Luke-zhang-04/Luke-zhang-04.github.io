bc75f18a95cf3bba6b3a87e13243391610f8513e
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-05 16:40:15 -0400

    force target="_blank" for markdown links

---
+++ src/components/markdown/anchor.svelte
@@ -0,0 +1,12 @@
+<!--
+Luke Zhang's developer portfolio | https://Luke-zhang-04.github.io
+
+License: BSD-3-Clause
+Copyright (C) 2020 - 2021 Luke Zhang
+ -->
+<script lang="ts">
+    export let href = ""
+    export let title: string | undefined = undefined
+</script>
+
+<a {href} {title} target="_blank" rel="noopener noreferrer"><slot /></a>
