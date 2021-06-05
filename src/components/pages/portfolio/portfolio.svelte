66a1964117a02fc649488f3b73db2d3e6fc50d80
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-05 14:32:01 -0400

    add tag filters

--- src/components/pages/portfolio/portfolio.svelte
+++ src/components/pages/portfolio/portfolio.svelte
@@ -4,6 +4,17 @@ Luke Zhang's developer portfolio | https://Luke-zhang-04.github.io
 License: BSD-3-Clause
 Copyright (C) 2020 - 2021 Luke Zhang
  -->
+<style lang="scss">
+    :global(.dropdown-menu.show) {
+        max-height: 50vh;
+        overflow-y: scroll;
+    }
+
+    .dropdown-item {
+        cursor: pointer;
+    }
+</style>
+
 <script lang="ts">
     import Fuse from "fuse.js/dist/fuse.basic.esm"
     import Grid from "./internal/grid.svelte"
@@ -20,9 +31,17 @@ Copyright (C) 2020 - 2021 Luke Zhang
 
     let allProjects: Project[] = []
     let projects: Project[] = []
+    let allTags = new Set(["all"])
+    let currentTag = "all"
 
     projectData.then((_projects) => {
         projects = allProjects = _projects
+
+        for (const project of projects) {
+            for (const tag of project.tags) {
+                allTags.add(tag)
+            }
+        }
     })
 
     const onInput: svelte.JSX.FormEventHandler<HTMLInputElement> = ({target}) => {
@@ -57,7 +76,39 @@ Copyright (C) 2020 - 2021 Luke Zhang
                 on:input={onInput}
             />
         </div>
-        <Grid {projects} />
+        <div class="dropdown col-1">
+            <button
+                class="btn input-group-text dropdown-toggle"
+                type="button"
+                id="tag-filter-dropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
+                <span class="material-icons">filter_list</span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="tag-filter-dropdown">
+                {#each Array.from(allTags) as tag}
+                    {#if currentTag === tag}
+                        <li class="dropdown-item d-flex flex-row justify-content-between">
+                            {tag}
+                            <span class="material-icons">check</span>
+                        </li>
+                    {:else}
+                        <li
+                            on:click={() => {
+                                currentTag = tag
+                            }}
+                            class="dropdown-item">{tag}</li
+                        >
+                    {/if}
+                {/each}
+            </ul>
+        </div>
+        <Grid
+            projects={currentTag === "all"
+                ? projects
+                : projects.filter((project) => project.tags.includes(currentTag))}
+        />
     </div>
 {:catch err}
     {#if err instanceof Error}
