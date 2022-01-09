cf53a9bb0bcbb2a5fb0967009d8f464c1ba5a752
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2022-01-08 21:16:06 -0500

    fix swiper

--- src/components/pages/home/swiper.svelte
+++ src/components/pages/home/swiper.svelte
@@ -4,30 +4,34 @@ Luke Zhang's developer portfolio | https://Luke-zhang-04.github.io
 License: BSD-3-Clause
 Copyright (C) 2020 - 2021 Luke Zhang
  -->
-
 <script lang="ts">
-    import "swiper/scss"
-    import "swiper/scss/pagination"
-    import {EffectCoverflow, Swiper as SwiperCore} from "swiper"
+    import "swiper/css"
+    import "swiper/css/pagination"
+    import {EffectCoverflow, Pagination} from "swiper"
     import {SwiperSlide as Slide, Swiper} from "swiper/svelte"
     import {imageMap, projectData, shouldUseLocalImages} from "~/globals"
     import {Link} from "svelte-routing"
     import Spinner from "~/components/spinner"
-
-    SwiperCore.use([EffectCoverflow])
 </script>
 
 {#await projectData}
     <Spinner color="primary" size="25vw" class="my-5" centered />
 {:then projects}
+    <div class="p-3 text-center mt-6 mt-md-0 title">
+        <div>
+            <h1>My Work</h1>
+            <Link to="portfolio" class="btn btn-outline-primary" id="see-all-btn">See all</Link>
+        </div>
+    </div>
     <Swiper
+        modules={[EffectCoverflow, Pagination]}
         effect="coverflow"
         loop
         grabCursor
         centeredSlides
         slidesPerView="auto"
         coverflowEffect={{
-            rotate: 50,
+            rotate: 45,
             stretch: 0,
             depth: 100,
             modifier: 1,
@@ -35,24 +39,12 @@ Copyright (C) 2020 - 2021 Luke Zhang
         }}
         pagination
     >
-        <div class="p-3 text-center mt-6 mt-md-0 title" slot="header">
-            <div>
-                <h1>My Work</h1>
-                <Link to="portfolio" class="btn btn-outline-primary" id="see-all-btn">
-                    See all
-                </Link>
-            </div>
-        </div>
-        <svelte:fragment slot="slides">
-            {#each projects as { name, imgUrl }}
-                <Slide
-                    loading="lazy"
-                    style="background-image: url({shouldUseLocalImages
-                        ? imageMap[name]
-                        : imgUrl});"
-                />
-            {/each}
-        </svelte:fragment>
+        {#each projects as { name, imgUrl }}
+            <Slide
+                loading="lazy"
+                style="background-image: url({shouldUseLocalImages ? imageMap[name] : imgUrl});"
+            />
+        {/each}
     </Swiper>
 {:catch err}
     {#if err instanceof Error}
--- src/images/index.ts
+++ src/images/index.ts
@@ -14,8 +14,11 @@ import kkcabinets from "./kkcabinets.svg"
 import leetcode from "./leetcode.svg"
 import processingVscode from "./processing-vscode.svg"
 import quickstart from "./quickstart.svg"
+import sleepstop from "./sleepstop.svg"
 import sortingAlgos from "./sorting-algos.svg"
 import talentmaker from "./talentmaker.svg"
+import utils from "./utils.svg"
+import vmeet from "./virtualmeet.svg"
 
 /* eslint-disable @typescript-eslint/naming-convention */
 const images: {[key: string]: string} = {
@@ -30,6 +33,9 @@ const images: {[key: string]: string} = {
     Talentmaker: talentmaker,
     "This Website": githubio,
     "sorting-algorithms": sortingAlgos,
+    SleepStop: sleepstop,
+    Utilities: utils,
+    VirtualMeet: vmeet,
 }
 /* eslint-disable @typescript-eslint/naming-convention */
 
