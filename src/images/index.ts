4262bb941e9ab817f14b76067c4ba9fb424ad679
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2023-10-07 19:03:22 -0400

    feat: add new items to portfolio

--- src/images/index.ts
+++ src/images/index.ts
@@ -9,15 +9,19 @@ import canvasPlus from "./canvasPlus.svg"
 import ccc from "./ccc.svg"
 import debateTimer from "./debate-timer.svg"
 import destagnate from "./destagnate.svg"
+import firefighter from "./firefighter.png"
 import githubio from "./githubio.svg"
 import jamhacks from "./jamhacks.svg"
 import kkcabinets from "./kkcabinets.svg"
 import leetcode from "./leetcode.svg"
+import loblawDigital from "./loblaw-digital.svg"
+import pillAid from "./pill-aid.svg"
 import processingVscode from "./processing-vscode.svg"
 import quickstart from "./quickstart.svg"
 import sleepstop from "./sleepstop.svg"
 import sortingAlgos from "./sorting-algos.svg"
 import talentmaker from "./talentmaker.svg"
+import trustiin from "./trustiin.svg"
 import utils from "./utils.svg"
 import vmeet from "./virtualmeet.svg"
 
@@ -27,17 +31,21 @@ const images: {[key: string]: string} = {
     CanvasPlus: canvasPlus,
     DeStagnate: destagnate,
     "Debate Timer Bot": debateTimer,
+    "Firefighter Bot": firefighter,
+    JAMHacks: jamhacks,
     "K&K Cabinets and Countertops": kkcabinets,
     "LeetCode-solutions": leetcode,
+    "Loblaw Digital": loblawDigital,
+    "Pill-Aid": pillAid,
     "Processing Vscode": processingVscode,
     Quickstart: quickstart,
+    SleepStop: sleepstop,
+    "sorting-algorithms": sortingAlgos,
     Talentmaker: talentmaker,
     "This Website": githubio,
-    "sorting-algorithms": sortingAlgos,
-    SleepStop: sleepstop,
+    TRUSTiiN: trustiin,
     Utilities: utils,
     VirtualMeet: vmeet,
-    JAMHacks: jamhacks,
 }
 /* eslint-disable @typescript-eslint/naming-convention */
 
